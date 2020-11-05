20c941c8ab729f2e39ac861f9d600cf7e0959d5a
Julen Sanchez Rosales <julenvitoria@hotmail.es>
2020-11-05 11:28:12 +0100

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Kelboy-SuperLopezGBTheme
-SuperLopezGBTheme and launchings
+# Kelboy-SuperRetroBoyTheme
+SuperRetroBoy Theme &amp; Launching images
+
+Script to install [Super RetroBoy theme by KALEL1981](https://github.com/KALEL1981/es-theme-Super-Retroboy) and launchings generateds for each systems 
+
+How to install 
+Connect via ssh or insert this command via terminal on your kelboy 
+
+wget -O- https://github.com/julenvitoria/Kelboy-SuperLopezGBTheme/SLGBtheme.sh | bash 
+
+
+NOTE: IT'S NECCESARY THAT YOUR KELBOY HAS INTERNET CONNECTION.
---
+++ SLGBtheme.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+cd /home/pi
+if [ -d "/home/pi/.emulationstation/themes/SuperLopezGB/" ]; then
+        echo "Super Lopez GB theme was downloaded yet"
+        echo "Deleting and redownloading..."
+        sleep 4
+        sudo rm -r "/home/pi/.emulationstation/themes/SuperLopezGB/"
+        sudo mkdir -p "/home/pi/.emulationstation/themes/SuperLopezGB/" && sudo git clone https://github.com/mlopezmad/SuperLopezGB "/home/pi/.emulationstation/themes/Super Retroboy/" --branch master --depth=1
+        #cd "/opt/retropie/configs/all/emulationstation/themes/Super Retroboy/"
+        #sudo sed -i -e 's/<fontSize>0.042/<fontSize>0.06/g' SuperRetroboy.xml
+        #sudo sed -i -e 's/<fontSize>0.025/<fontSize>0.035/g' SuperRetroboy.xml
+        #sudo sed -i -e 's/<fontSize>0.04/<fontSize>0.055/g' SuperRetroboy.xml
+else
+        echo "Downloading Super Lopez GB theme..."
+        sleep 4
+        sudo mkdir -p "/home/pi/.emulationstation/themes/SuperLopezGB/" && sudo git clone https://github.com/mlopezmad/SuperLopezGB "/home/pi/.emulationstation/themes/Super Retroboy/" --branch master --depth=1
+        #cd "/opt/retropie/configs/all/emulationstation/themes/Super Retroboy/"
+        #sudo sed -i -e 's/<fontSize>0.042/<fontSize>0.06/g' SuperRetroboy.xml
+        #sudo sed -i -e 's/<fontSize>0.025/<fontSize>0.035/g' SuperRetroboy.xml
+        #sudo sed -i -e 's/<fontSize>0.04/<fontSize>0.055/g' SuperRetroboy.xml
+fi
+
+#make temporal directory
+cd /home/pi
+if [ -d /home/pi/tmp ]; then
+        sudo rm -R /home/pi/tmp
+        mkdir /home/pi/tmp
+else
+        mkdir /home/pi/tmp
+fi
+#Download and install launching images
+git clone https://github.com/mlopezmad/SuperLopezGB-Launching "/home/pi/tmp" --branch master --depth=1
+echo "COPYING LAUNCHING IMAGES..."
+sleep 3
+cp -R /home/pi/tmp/* /opt/retropie/configs
+sudo rm -r tmp
+#Change theme value on EmulationStation config file
+#sed -i 's/.*<string name="ThemeSet" value=.*/<string name="ThemeSet" value="SuperLopezGB" /> \/>/' /opt/retropie/configs/all/emulationstation/es_settings.cfg
+#Restart EmulationStation
+/home/pi/scripts/multi_switch.sh --ES-RESTART
